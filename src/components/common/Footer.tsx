a52a2b36b3a480aa2a8d2c612abac37b318f4935
sakhawat9 <sakhawathossain7969@gmail.com>
2021-11-13 12:26:16 +0600

    [ADD] Header done

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,8 +2,16 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <h2>This is a footer</h2>
+    <div className="py-12">
+      <div className="container mx-auto">
+        <div className="flex justify-center">
+          <p>
+            &copy; 2021{" "}
+            <a href="https://github.com/sakhawat9">Cheesy_kitchen</a> Designed
+            by SH Shakib
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,62 @@
 module.exports = {
-  mode: 'jit',
   purge: ["./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false, // or 'media' or 'className'
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        sm: "100%",
+        md: "100%",
+        lg: "1024px",
+        xl: "1280px",
+      },
+    },
+    fontFamily: {
+      roboto: ["Roboto"],
+      lora: ["Lora"],
+    },
+    extend: {
+      colors: {
+        "saffron": {
+          DEFAULT: "#f4c340",
+          50: "#fefcf5",
+          100: "#fef9ec",
+          200: "#fcf0cf",
+          300: "#fbe7b3",
+          400: "#f7d579",
+          500: "#f4c340",
+          600: "#dcb03a",
+          700: "#b79230",
+          800: "#927526",
+          900: "#78601f",
+        },
+        "amazon": {
+          DEFAULT: "#347758",
+          50: "#f5f8f7",
+          100: "#ebf1ee",
+          200: "#ccddd5",
+          300: "#aec9bc",
+          400: "#71a08a",
+          500: "#347758",
+          600: "#2f6b4f",
+          700: "#275942",
+          800: "#1f4735",
+          900: "#193a2b",
+        },
+        "theme-yellow": "#ffc727",
+        "theme-yellow-dark": "#e6b323",
+        "theme-dark": "#37474f",
+      },
+
+      height: {
+        "screen-75": "80vh",
+        "screen-50": "50vh",
+      },
+    },
   },
+
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/forms", "@tailwindcss/aspect-ratio")],
+};
