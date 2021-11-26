6ff05e715e09ec46174f4a9a1fd4858fb6101588
sakhawat9 <sakhawathossain7969@gmail.com>
2021-11-26 11:36:42 +0600

    [ADD] css

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,18 +1,24 @@
 import "animate.css/animate.min.css";
-import { useForm } from "react-hook-form";
 import axios, { AxiosRequestConfig } from "axios";
+import Layout from "components/common/Layout";
+import Title from "components/common/Title";
 import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
 import { cssTransition, toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-
 const zoomIn = cssTransition({
-    enter: "animate__animated animate__zoomIn",
-    exit: "animate__animated animate__zoomIn",
-  });
+  enter: "animate__animated animate__zoomIn",
+  exit: "animate__animated animate__zoomIn",
+});
 
 export default function Home() {
-  const { register, handleSubmit, formState: { errors }, reset } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
   const router = useRouter();
   async function onSubmitForm(values: any): Promise<void> {
     let config: AxiosRequestConfig = {
@@ -40,16 +46,15 @@ export default function Home() {
   }
 
   return (
-    <div className="max-w-full p-8 m-8 bg-white rounded-md">
-        <ToastContainer />
-        <form
-          onSubmit={handleSubmit(onSubmitForm)}
-          className="grid grid-cols-1 gap-y-6"
-        >
-          <div>
-            <label htmlFor="name" className="sr-only">
-              Full Name
-            </label>
+    <Layout>
+      <ToastContainer />
+      <div className="register">
+        <div className="register__wrapper">
+          <Title title="Contact Us" subtitle="" description="" />
+        </div>
+        <form onSubmit={handleSubmit(onSubmitForm)} className="register__form">
+          <label>
+            <span className="register__form__title">Name</span>
             <input
               type="text"
               name="name"
@@ -59,19 +64,18 @@ export default function Home() {
                   message: "You most enter name",
                 },
               })}
-              className={`block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow focus:ring-blue-500 focus:border-blue-500 focus:outline-none focus:ring-2
-               ${errors.name ? 'ring-2 ring-red-500' : null}`}
-               placeholder="Full name"
+              className={`${errors.name ? "ring-2 ring-red-500" : null}`}
+              placeholder="Full name"
             />
-            <span className="py-2 text-sm text-red-400">{errors?.name?.message}</span>
-          </div>
-          <div>
-            <label htmlFor="email" className="sr-only">
-              Email
-            </label>
+            <span className="py-2 text-sm text-red-400">
+              {errors?.name?.message}
+            </span>
+          </label>
+          <label>
+            <span className="register__form__title">Email</span>
             <input
               type="email"
-              name="email"
+              name="Email"
               {...register("email", {
                 required: {
                   value: true,
@@ -86,69 +90,62 @@ export default function Home() {
                   message: "This is too long",
                 },
                 pattern: {
-                  value: /\S+@\S+\.\S+/,
-                  message: "invalid email address"
-                }
+                  value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+                  message: "invalid email address",
+                },
               })}
-              className={`block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow focus:ring-blue-500 focus:border-blue-500 focus:outline-none focus:ring-2
-               ${errors.email ? 'ring-2 ring-red-500' : null}`}
+              className={`${errors.email ? "ring-2 ring-red-500" : null}`}
               placeholder="Email"
             />
-            <span className="py-2 text-sm text-red-400">{errors?.email?.message}</span>
-          </div>
-          <div>
-            <label htmlFor="phone" className="sr-only">
-              Phone
-            </label>
+            <span className="py-2 text-sm text-red-400">
+              {errors?.email?.message}
+            </span>
+          </label>
+
+          <label>
+            <span className="register__form__title">Phone</span>
             <input
               type="text"
               name="phone"
               {...register("phone")}
-              className="block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow focus:ring-blue-500 focus:border-blue-500 focus:outline-none focus:ring-2"
               placeholder="Phone"
             />
-          </div>
-          <div>
-            <label htmlFor="message" className="sr-only">
-              Message
-            </label>
+          </label>
+
+          <label>
+            <span className="register__form__title">Message</span>
+
             <textarea
               name="message"
               {...register("message", {
                 required: {
                   value: true,
-                  message: 'You need to enter your message'
+                  message: "You need to enter your message",
                 },
                 maxLength: {
                   value: 1000,
-                  message: "You message can't be more than 1000 characters"
+                  message: "You message can't be more than 1000 characters",
                 },
                 minLength: {
                   value: 50,
-                  message: "You message must be longer then this!"
-                }
+                  message: "You message must be longer then this!",
+                },
               })}
               className={`block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow focus:ring-blue-500 focus:border-blue-500 focus:outline-none focus:ring-2
-               ${errors.message ? 'ring-2 ring-red-500' : null}`}
+               ${errors.message ? "ring-2 ring-red-500" : null}`}
               placeholder="Message"
             />
-            <span className="py-2 text-sm text-red-400">{errors?.message?.message}</span>
-          </div>
-          <div>
-            <button
-              type="submit"
-              className="px-16 py-2 my-2 text-white rounded-full bg-gradient-to-r from-green to-blue-400 focus:outline-none"
-            >
-              Submit
-            </button>
-          </div>
+            <span className="py-2 text-sm text-red-400">
+              {errors?.message?.message}
+            </span>
+          </label>
+          <input
+            type="submit"
+            className="w-full mt-4 text-white rounded bg-saffron-600"
+            value="Submit"
+          />
         </form>
-    </div>
+      </div>
+    </Layout>
   );
 }
-
-
-
-
-
-
