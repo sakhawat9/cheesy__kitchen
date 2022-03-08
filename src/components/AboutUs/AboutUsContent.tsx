f983b1696fd5e3263f45a50595e3c3415fe1eef9
sakhawat9 <sakhawathossain7969@gmail.com>
2022-03-08 16:15:51 +0600

    [ADD] 404 page

---
+++ src/components/AboutUs/AboutUsContent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutUsContent = () => {
+    return (
+        <div>
+            <h2>This is about_us_content</h2>
+        </div>
+    );
+};
+
+export default AboutUsContent;
---
+++ src/pages/aboutUs.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AboutUsContent from "components/AboutUs/AboutUsContent";
+
+const aboutUs = () => {
+  return (
+    <div>
+      <AboutUsContent />
+    </div>
+  );
+};
+
+export default aboutUs;
